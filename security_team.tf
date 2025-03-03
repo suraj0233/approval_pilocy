53904bafb2c9749ea446559e9bc30f2bb8e7d587
suraj0233 <160613987+suraj0233@users.noreply.github.com>
2025-03-03 18:10:38 +0530

    Update security_team.tf

--- security_team.tf
+++ security_team.tf
@@ -1,5 +1,5 @@
 resource "spacelift_mounted_file" "security_team" {
-  stack_id      = "*"  # This will mount the file to all stacks
+  stack_id      = "sample"  # This will mount the file to all stacks
   relative_path = "security_team.rego"
   content       = <<EOT
 package spacelift
